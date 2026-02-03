6ea5ceac1bec86e1fb51870a39992a795a7a08b3
Preston Macy <preston.macy@gmail.com>
2026-02-03 15:38:46 -0500

    refactor: moved start and stop polling to global functions, changed intersection active check logic

--- light-controller-ui/src/components/Intersection.vue
+++ light-controller-ui/src/components/Intersection.vue
@@ -5,37 +5,39 @@ import axios from 'axios'
 import type { Intersection, Road, Light } from '../types/'
 import TrafficLight from './TrafficLight.vue'
 import { global } from '../stores/appState'
-import { isIntersectionActive } from '../services'
-import { activateIntersection, deactivateIntersection, createIntersection } from '../services'
+import { activateIntersection, deactivateIntersection, createIntersection, fetchIntersectionState } from '../services'
+
+const { stopPolling, startPolling } = usePolling(fetchIntersectionState, 50)
+global.stopPolling = stopPolling
+global.startPolling = startPolling
 
 onMounted(async () => {
   await createIntersection()
-  { startPolling, stopPolling } = usePolling(fetchIntersectionState, 5000)
 })
 
 onUnmounted(() => {
-  stopPolling()
+  global.stopPolling()
 })
 </script>
 <template>
   <div class='intersection'>
     <div class='north-light'>
-      <TrafficLight />
+      <TrafficLight :light="global.intersectionData.value?.roads[0]?.lights[0]" />
     </div>
     <div class='east-west-lights'>
-      <TrafficLight />
-      <TrafficLight />
+      <TrafficLight :light="global.intersectionData.value?.roads[1]?.lights[0]" />
+      <TrafficLight :light="global.intersectionData.value?.roads[1]?.lights[1]" />
     </div>
     <div class='south-light'>
-      <TrafficLight />
+      <TrafficLight :light="global.intersectionData.value?.roads[0]?.lights[1]" />
     </div>
   </div>
   <div class='footer'>
-    <div v-if="isIntersectionActive">
-      <button @click="activateIntersection">Activate Intersection</button>
+    <div v-if="global.intersectionData.value?.active">
+      <button @click="deactivateIntersection">DeActivate Intersection</button>
     </div>
     <div v-else>
-      <button @click="deactivateIntersection">DeActivate Intersection</button>
+      <button @click="activateIntersection">Activate Intersection</button>
     </div>
   </div>
 </template>
