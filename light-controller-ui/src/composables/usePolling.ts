40dbb4d3b3ae72512902b4b5e9f8910eae8169c5
Preston Macy <preston.macy@gmail.com>
2026-02-03 15:35:47 -0500

    update: removed starting polling on mount

--- light-controller-ui/src/composables/usePolling.ts
+++ light-controller-ui/src/composables/usePolling.ts
@@ -3,15 +3,6 @@ import { onMounted, onUnmounted } from 'vue'
 export const usePolling = (cb: () => void, interval: number) => {
   let pollingInterval: number | null = null
 
-  onMounted(() => {
-    cb()
-    pollingInterval = setInterval(cb, interval)
-  })
-
-  onUnmounted(() => {
-    if (pollingInterval) clearInterval(pollingInterval)
-  })
-
   const stopPolling = () => {
     if (pollingInterval) {
       clearInterval(pollingInterval)
