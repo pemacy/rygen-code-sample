893e8742cada6e977125e1fbd48a79dec45812ed
Preston Macy <preston.macy@gmail.com>
2026-02-03 12:27:17 -0500

    refactor: changed main vue file to reference components

--- light-controller-ui/src/App.vue
+++ light-controller-ui/src/App.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
-const activeLight = ref<string>('red')
-
-const handleActiveLightChange = () => {
-  axios.post('http://localhost:8080/intersections', { activeLight: activeLight.value })
-    .then(console.log)
-    .catch(console.error)
-}
+import IntersectionView from './components/Intersection.vue'
 </script>
 
 <template>
@@ -18,72 +10,30 @@ const handleActiveLightChange = () => {
   </header>
 
   <main>
-    <div class="light-controller">
-      <div class="light">
-        <label>
-          <input type="radio" value="red" class="red" v-model="activeLight" name="light"
-            @change="handleActiveLightChange" /> Red
-        </label>
-        <label>
-          <input type="radio" value="yellow" class="yellow" v-model="activeLight" name="light"
-            @change="handleActiveLightChange" /> Yellow
-        </label>
-        <label>
-          <input type="radio" value="green" class="green" v-model="activeLight" name="light"
-            @change="handleActiveLightChange" /> Green
-        </label>
-      </div>
-
-      <p>Active light: {{ activeLight }}</p>
-    </div>
+    <IntersectionView />
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  padding: 1rem 0;
+  border-bottom: 1px solid #e0e0e0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    margin: calc(var(--section-gap) / 4);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  font-size: 2rem;
+  color: #333;
+  margin: 0;
 }
 
-.light-controller {
-  display: grid;
-  place-items: center;
-  gap: 1rem;
-
-  .light {
-    display: grid;
-    gap: .5rem;
-  }
-
-}
-
-input[type='radio'].red {
-  accent-color: #cc3232;
-}
-
-input[type='radio'].yellow {
-  accent-color: #e7b416;
-}
-
-input[type='radio'].green {
-  accent-color: #2dc937;
+main {
+  padding: 2rem 0;
 }
 </style>
