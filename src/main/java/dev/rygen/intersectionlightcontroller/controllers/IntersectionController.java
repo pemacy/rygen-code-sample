0facc8485dd319f5454981fa34006aaed77ab7be
Preston Macy <preston.macy@gmail.com>
2026-02-03 06:41:57 -0500

    feat: created paths for intersection controller

--- src/main/java/dev/rygen/intersectionlightcontroller/controllers/IntersectionController.java
+++ src/main/java/dev/rygen/intersectionlightcontroller/controllers/IntersectionController.java
@@ -1,27 +1,63 @@
 package dev.rygen.intersectionlightcontroller.controllers;
 
+import dev.rygen.intersectionlightcontroller.dtos.ChangeLightColorRequest;
 import dev.rygen.intersectionlightcontroller.dtos.IntersectionDTO;
 import dev.rygen.intersectionlightcontroller.entities.Intersection;
 import dev.rygen.intersectionlightcontroller.services.IntersectionService;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/intersections")
 public class IntersectionController {
 
-    private final IntersectionService intersectionService;
+  private final IntersectionService intersectionService;
 
-    public IntersectionController(IntersectionService intersectionService) {
-        this.intersectionService = intersectionService;
-    }
+  public IntersectionController(IntersectionService intersectionService) {
+    this.intersectionService = intersectionService;
+  }
 
-    @PostMapping
-    public void createIntersection(@RequestBody IntersectionDTO intersectionDto) {
-        Intersection intersection = Intersection.builder()
-                .activeLight(intersectionDto.activeLight())
-                .build();
-        this.intersectionService.createIntersection(intersection);
-    }
+  @PostMapping
+  public ResponseEntity<IntersectionDTO> createIntersection() {
+    Intersection intersection = intersectionService.createIntersection();
+    IntersectionDTO dto = IntersectionDTO.fromEntity(intersection);
+    return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<IntersectionDTO> getIntersection(@PathVariable Long id) {
+    return intersectionService.getIntersection(id)
+        .map(IntersectionDTO::fromEntity)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping("/{id}/activate")
+  public ResponseEntity<IntersectionDTO> activateIntersection(@PathVariable Long id) {
+    return intersectionService.activateIntersection(id)
+        .map(IntersectionDTO::fromEntity)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping("/{id}/deactivate")
+  public ResponseEntity<IntersectionDTO> deactivateIntersection(@PathVariable Long id) {
+    return intersectionService.deactivateIntersection(id)
+        .map(IntersectionDTO::fromEntity)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{id}/lights")
+  public ResponseEntity<IntersectionDTO> changeLightColor(
+      @PathVariable Long id,
+      @RequestBody ChangeLightColorRequest request) {
+    return intersectionService.changeLightColor(id, request.lightId(), request.newColor())
+        .map(IntersectionDTO::fromEntity)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
 }
