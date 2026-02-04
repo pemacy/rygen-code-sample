51c3c6138a1eef03ea50f53ae60dfd16a7277aab
Preston Macy <preston.macy@gmail.com>
2026-02-04 08:16:53 -0500

    feat: added OFF to light color options

--- src/main/java/dev/rygen/intersectionlightcontroller/enums/LightColor.java
+++ src/main/java/dev/rygen/intersectionlightcontroller/enums/LightColor.java
@@ -3,21 +3,24 @@ package dev.rygen.intersectionlightcontroller.enums;
 public enum LightColor {
   GREEN,
   YELLOW,
-  RED;
+  RED,
+  OFF;
 
-  public LightColor next () {
+  public LightColor next() {
     return switch (this) {
       case GREEN -> YELLOW;
       case YELLOW -> RED;
       case RED -> GREEN;
+      case OFF -> OFF;
     };
   }
 
-  public LightColor opposite () {
-    return switch(this) {
+  public LightColor opposite() {
+    return switch (this) {
       case GREEN -> RED;
       case RED -> GREEN;
-      case YELLOW -> YELLOW;
+      case YELLOW -> RED;
+      case OFF -> OFF;
     };
   }
 }
